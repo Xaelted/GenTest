0558c4a73a14b3ebd0bcb9bf8e2aff587e28692f
Xaelted <dragon.hunter.ow@gmail.com>
2017-01-19 14:57:50 -0600

    :)

--- Main.java
+++ Main.java
@@ -3,7 +3,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("this is a test");
+		Circle Circle1 = new Circle (5);
+		System.out.println("The area of Circle 1 is " +Circle1.getArea());
+		
+		
+		
 		
 	}
 
 }
+
+
