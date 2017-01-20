1037fef9de7c6450a6b34dee8878d6f7e4796781
Xaelted <dragon.hunter.ow@gmail.com>
2017-01-19 18:08:26 -0600

    boop

--- BiomesGen.java
+++ BiomesGen.java
@@ -14,7 +14,7 @@ public class BiomesGen {
 		
 		if(BiomeName.equals("Plains")){
 			for(int r = 0; r< Map.length; r++){
-				for (int c = 0; c< Map[r].length; c++){
+				for (int c = 1; c< Map[r].length; c++){
 					Map[r][c] = GenPlains.GetRealHeight(Map, r, c);
 					
 				}
@@ -30,22 +30,22 @@ public class BiomesGen {
 	public static int FindAvgHeight (int[][] Heightmap, int r, int c) {
 		
 		if (r<=0 && c <=0){
-			AvgHeight = (Heightmap[r][c+1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/8;
+			AvgHeight = (Heightmap[r][c+1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/3;
 		}
 		else if (r>=Heightmap.length && c>=Heightmap[r].length){
-			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r][c-1])/8;
+			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r][c-1])/3;
 		}
 		else if (r>=Heightmap.length){
-			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r-1][c+1]+Heightmap[r][c-1]+Heightmap[r][c+1])/8;
+			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r-1][c+1]+Heightmap[r][c-1]+Heightmap[r][c+1])/5;
 		}
 		else if (c>=Heightmap.length){
-			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r][c-1]+Heightmap[r+1][c-1]+Heightmap[r+1][c])/8;
+			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r][c-1]+Heightmap[r+1][c-1]+Heightmap[r+1][c])/5;
 		}
 		else if(r<0) {
-			AvgHeight = (Heightmap[r][c-1]+Heightmap[r][c+1]+Heightmap[r+1][c-1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/8;
+			AvgHeight = (Heightmap[r][c-1]+Heightmap[r][c+1]+Heightmap[r+1][c-1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/5;
 		}
 		else if (c<0) {
-			AvgHeight = (Heightmap[r-1][c]+Heightmap[r-1][c+1]+Heightmap[r][c+1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/8;
+			AvgHeight = (Heightmap[r-1][c]+Heightmap[r-1][c+1]+Heightmap[r][c+1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/5;
 		}
 		else {
 			AvgHeight = (Heightmap[r-1][c-1]+Heightmap[r-1][c]+Heightmap[r-1][c+1]+Heightmap[r][c-1]+Heightmap[r][c+1]+Heightmap[r+1][c-1]+Heightmap[r+1][c]+Heightmap[r+1][c+1])/8;
